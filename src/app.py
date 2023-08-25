d247a2fbd3db3aae0686177fefeeb66d963030b6
hqt08 <tayhuiqithq@gmail.com>
2023-08-24 19:48:27 -0800

    Input

--- src/app.py
+++ src/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Flask, Test. Build 2</p>"
+def home():
+    return """
+              <form action='/user_input' method='POST'>
+              <input name="input">
+              <input type="submit" value="Send">
+              </form>
+    
+            """
+
+@app.route("/user_input", methods=["POST"])
+def input():
+   
+   if "input" not in request.form:
+      return(404, "input not found")
+   return (f"Input: {request.form['input']}")
 
 if __name__ == "__main__":
   app.run(debug = True)
