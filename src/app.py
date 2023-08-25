5a49f8833e468006f124d62f0cf9af80a95d19aa
hqt08 <tayhuiqithq@gmail.com>
2023-08-24 19:14:14 -0800

    less excite

--- src/app.py
+++ src/app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Flask, Test!</p>"
-
+    return "<p>Flask, Test.</p>"
 
 if __name__ == "__main__":
   app.run(debug = True)
